ee2daa564db8a877d590e2445e8acdd92f5abab8
Dhruv kumar <vas.dhruv79@gmail.com>
2026-03-26 21:04:29 +0530

    another lecture complete in javascript

---
+++ 02_datatype.js
@@ -0,0 +1,22 @@
+"use strict";  //treat all js code as newer version
+
+// alert(3 + 4); //7  but we are using nodejs not browser
+
+console.log(3 + 4); // code readibility is more important than code brevity
+
+let name = "John" //string literal
+let age = 19 //number literal
+let IsloggedIn = false //boolean literal
+
+// number => 2 to power 53
+//bigint
+//string = ""
+//boolean = true/false
+//null => empty value/standalone value
+//undefined => value is not assigned
+//symbol => unique
+
+//object
+
+console.log(typeof "undefined"); //undefined
+console.log(typeof null); //object => bug in js but we can't change it because of backward compatibility
